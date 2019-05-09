34decd42438d0f7477c808a759a9b64d750f3bfd
BCH2524 <50466917+BCH2524@users.noreply.github.com>
2019-05-09 19:20:19 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_BrandonCarlton-Hanks.py
@@ -0,0 +1,14 @@
+
+
+# I am programing a sales prediction in python to display the profit.
+# February 11, 2019
+# CTI-110 P2T1 - Sales Prediction
+# Brandon Carlton-Hanks
+# Get the projected total sales.
+total_sales = float( input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
